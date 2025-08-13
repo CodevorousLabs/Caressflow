b4733df2802cacf73529cd64992145bebcb2b746
Güven Fazlı <guven.fazli@gmail.com>
2025-08-13 04:08:17 +0300

    style: responsive design and padding added to stats component

--- client/components/landingPageComponents/stats/stats.tsx
+++ client/components/landingPageComponents/stats/stats.tsx
@@ -1,20 +1,39 @@
 export default function Stats() {
   return (
-    <div className="flex relative w-full justify-center items-center h-0">
-      <div className="flex items-center justify-around gap-x-5 bg-[#00ACB0] w-8/12 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20 min-h-[200px]">
-        <div className="text-white text-center font-rubik space-y-5">
+    <div className="relative w-full max-lg:h-[250px]">
+      <div className="
+        absolute 
+        left-1/2 top-1/2 
+        -translate-x-1/2 -translate-y-1/2 
+
+        bg-[#00ACB0] 
+        w-10/12 max-w-6xl 
+        min-h-[200px]
+
+        grid
+        grid-cols-1
+        gap-x-6 gap-y-6
+        justify-items-center
+        items-center
+
+        md:grid-cols-2
+        lg:grid-cols-4
+
+        max-lg:py-8
+      ">
+        <div className="text-white text-center font-rubik space-y-3">
           <p className="text-5xl font-medium text-pink-600">5400+</p>
           <p className="text-xl">İyileşen Hastalar</p>
         </div>
-        <div className="text-white text-center font-rubik space-y-5">
+        <div className="text-white text-center font-rubik space-y-3">
           <p className="text-5xl font-medium text-pink-600">220+</p>
           <p className="text-xl">Uygulama Klinikleri</p>
         </div>
-        <div className="text-white text-center font-rubik space-y-5">
+        <div className="text-white text-center font-rubik space-y-3">
           <p className="text-5xl font-medium text-pink-600">5+</p>
           <p className="text-xl">Yıllık Deneyim</p>
         </div>
-        <div className="text-white text-center font-rubik space-y-5">
+        <div className="text-white text-center font-rubik space-y-3">
           <p className="text-5xl font-medium text-pink-600">380+</p>
           <p className="text-xl">Doktorlar ve Uzmanlar</p>
         </div>
