98e5f77f6ebd09e89b3d0b6ba9981959c7e2214b
Güven Fazlı <guven.fazli@gmail.com>
2025-08-13 06:49:13 +0300

    fix: visual bug (overflow) fixed

--- client/components/landingPageComponents/informationSection/contactInformation.tsx
+++ client/components/landingPageComponents/informationSection/contactInformation.tsx
@@ -9,23 +9,24 @@ export default function ContactInformation() {
       max-lg:flex-col
       max-lg:items-start
       max-lg:gap-5
+      max-[380px]:px-5
     ">
 
-      <div className="flex w-1/4 items-center space-x-7 max-lg:w-full">
+      <div className="flex w-1/4 items-center space-x-7 max-lg:w-full max-[380px]:space-x-2">
         <div className="p-2 border border-[##EBF2F3] rounded-full">
           <MdOutlineLocationOn className="text-pink-bg overflow-visible text-lg" />
         </div>
         <p className="text-[#686A6F]">Ataköy 7-8-9-10. Kısım Mahallesi, Çobançeşme E-5 Yanyol Caddesi, Ataköy Towers 20-A/52 Bakırköy/İstanbul</p>
       </div>
 
-      <div className="flex items-center space-x-7">
+      <div className="flex items-center space-x-7 max-[380px]:space-x-2">
         <div className="p-2 border border-[##EBF2F3] rounded-full">
           <FiPhone className="text-pink-bg overflow-visible text-lg" />
         </div>
         <a href="tel:905375224051" className="text-pink-bg">0537 522 40 51</a>
       </div>
 
-      <div className="flex items-center space-x-7">
+      <div className="flex items-center space-x-7 max-[380px]:space-x-2">
         <div className="p-2 border border-[##EBF2F3] rounded-full">
           <FaRegEnvelope className="text-pink-bg overflow-visible text-lg max" />
         </div>
