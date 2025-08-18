d43fa9403fa1fb2013ff18fa6fede697b8c5ba33
Güven Fazlı <guven.fazli@gmail.com>
2025-08-18 14:36:25 +0300

    refactor: responsive added

--- client/components/doctorBenefits/imageSection.tsx
+++ client/components/doctorBenefits/imageSection.tsx
@@ -3,13 +3,36 @@ import Image from 'next/image'
 import doctorTechSheet from '@/assets/doctorBenefits/technicSheet.jpg'
 import fadPic from '@/assets/doctorBenefits/fadPic.png'
 import SectionDescription from '../landingPageComponents/sectionDescription'
+
 export default function ImageSection() {
   return (
-    <div className='w-full md:w-10/12 lg:w-7/12 mx-auto flex flex-col justify-start items-start py-12 md:py-20 lg:py-[100px] gap-8 md:gap-12 lg:gap-14 px-4 text-center'>
-      <Image src={doctorTechSheet} alt='doctorTechImage' />
+    <div className='w-full md:w-10/12 lg:w-7/12 mx-auto flex flex-col justify-start items-center py-12 md:py-20 lg:py-[100px] gap-8 md:gap-12 lg:gap-14 px-4 md:px-6 lg:px-0 text-center'>
+
+   
+      <div className='w-full'>
+        <Image
+          src={doctorTechSheet}
+          alt='doctorTechImage'
+          className='w-full h-auto rounded-md object-cover'
+          sizes="(max-width: 768px) 100vw,
+                 (max-width: 1024px) 80vw,
+                 60vw"
+        />
+      </div>
+
       <SectionDescription>Caress Flow ECM Kursunu (FAD) Keşfedin</SectionDescription>
+
       <Link href={'/'}>
-        <Image src={fadPic} alt='fadPic' />
+        <div className='w-full md:w-8/12 lg:w-6/12 mx-auto'>
+          <Image
+            src={fadPic}
+            alt='fadPic'
+            className='w-full h-auto object-contain'
+            sizes="(max-width: 768px) 100vw,
+                   (max-width: 1024px) 70vw,
+                   50vw"
+          />
+        </div>
       </Link>
     </div>
   )
