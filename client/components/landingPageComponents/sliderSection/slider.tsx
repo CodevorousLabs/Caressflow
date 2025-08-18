6c7f0bd7a8920444b43c6a2268ff59d0bf47fc0a
Güven Fazlı <guven.fazli@gmail.com>
2025-08-18 18:02:08 +0300

    style: ShadCN carousel being used for slider/carousel, video added as giff, autoplay and loop activated

--- client/components/landingPageComponents/sliderSection/slider.tsx
+++ client/components/landingPageComponents/sliderSection/slider.tsx
@@ -1,6 +1,14 @@
 'use client'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 import { useState } from "react"
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import Image from "next/image";
 import lale from "../../../assets/lale.jpg";
 import ilac from "@/assets/ilac.jpg";
 import caressFlowDevrimi from "@/assets/caressFlowDevrimi.jpg";
@@ -45,63 +53,63 @@ export default function Slider() {
   }
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      className="relative w-full h-[830px] overflow-hidden font-heebo">
+    <Carousel className="w-full h-[830px] relative overflow-hidden">
+      <CarouselContent className="flex h-[830px]">
 
-      {sliderIndex > 0 && <div className="absolute top-0 bottom-0 right-0 left-0 bg-[#930b5c]/40 z-10" />}
+        {sliderData.map((asset, index) => (
+          <CarouselItem key={index} className="relative h-[830px] flex justify-center items-center">
+            {index > 0 &&
+              <>
+                <div className="absolute inset-0 bg-[#930b5c]/40 z-10" />
+                <Image
+                  src={asset.imageUrl}
+                  alt="asset"
+                  fill
+                  className="object-cover absolute"
+                />
+              </>
+            }
 
-      {sliderIndex === 0 ?
-        <video controls autoPlay={true} loop={true} className="absolute w-full inset-0 sm:bg-top transition-all duration-700">
-          <source src="/assets/sliderVideo.mp4" type="video/mp4" />
-        </video>
-        :
-        <motion.div
-          className="absolute inset-0 bg-cover bg-center sm:bg-top transition-all duration-700"
-          style={{
-            backgroundImage: `url(${sliderData[sliderIndex].imageUrl.src})`,
-          }} />
-      }
+            {index === 0 &&
+              <video autoPlay={true} loop={true} muted className="absolute w-full inset-0 sm:bg-top">
+                <source src="/assets/sliderVideo.mp4" type="video/mp4" />
+              </video>
+            }
 
 
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4 space-y-2">
-        <div className="flex flex-col w-full justify-around space-y-20 h-1/2 items-center z-40">
-          <div className="space-y-4 sm:space-y-8">
-            <p className="text-sm sm:text-base md:text-lg lg:text-xl">
-              {sliderData[sliderIndex].title}
-            </p>
-            <p className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight">
-              {sliderData[sliderIndex].description}
-            </p>
-          </div>
+            <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4 space-y-2">
+              <div className="flex flex-col w-full justify-around space-y-20 h-1/2 items-center z-40">
+                <div className="space-y-4 sm:space-y-8">
+                  <p className="text-sm sm:text-base md:text-lg lg:text-xl">
+                    {asset.title}
+                  </p>
+                  <p className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight">
+                    {asset.description}
+                  </p>
+                </div>
 
-          <div className="w-full sm:w-3/5 md:w-1/2 lg:w-1/6">
-            <a
-              href={sliderData[sliderIndex].anchorData}
-              className="bg-[#00ACB1] hover:bg-[#00787b] w-full transition p-3 sm:p-4 rounded-lg shadow-lg text-white font-semibold text-xs sm:text-sm md:text-base cursor-pointer block"
-            >
-              <button className="w-full font-bold cursor-pointer">Daha Fazla Bilgi +</button>
-            </a>
-          </div>
-        </div>
-      </div>
+                <div className="w-full sm:w-3/5 md:w-1/2 lg:w-1/6">
+                  <a
+                    href={asset.anchorData}
+                    className="bg-[#00ACB1] hover:bg-[#00787b] w-full transition p-3 sm:p-4 rounded-lg shadow-lg text-white font-semibold text-xs sm:text-sm md:text-base cursor-pointer block"
+                  >
+                    <button className="w-full font-bold cursor-pointer">Daha Fazla Bilgi +</button>
+                  </a>
+                </div>
+              </div>
+            </div>
 
-      <button
-        onClick={prevSlide}
-        className="absolute left-4 top-1/2 z-20 -translate-y-1/2 bg-[#00ACB1] hover:bg-[#00787b] p-3 rounded-full shadow-lg text-white cursor-pointer"
-        aria-label="Previous Slide"
-      >
-        <FaChevronLeft size={20} />
-      </button>
-      <button
-        onClick={nextSlide}
-        className="absolute right-4 top-1/2 z-20 -translate-y-1/2 bg-[#00ACB1] hover:bg-[#00787b] p-3 rounded-full shadow-lg text-white cursor-pointer"
-        aria-label="Next Slide"
-      >
-        <FaChevronRight size={20} />
-      </button>
-    </motion.div>
+          </CarouselItem>
+        ))}
+
+      </CarouselContent>
+      <CarouselPrevious
+        className="absolute left-4 top-1/2 -translate-y-1/2 bg-[#00ACB1] hover:bg-[#00787b] p-3 rounded-full shadow-lg text-white cursor-pointer"
+      />
+
+      <CarouselNext
+        className="absolute right-4 top-1/2 -translate-y-1/2 bg-[#00ACB1] hover:bg-[#00787b] p-3 rounded-full shadow-lg text-white cursor-pointer"
+      />
+    </Carousel>
   )
 }
